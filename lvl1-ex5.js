cc8435189140d56272065072de8bb05e20a2c34f
Samir Soula <samirsl@MacBook-de-Samir.local>
2025-05-17 01:05:40 +0200

    Ajout de l'exercice 5 : Somme des nombres dans un tableau

---
+++ lvl1-ex5.js
@@ -0,0 +1,7 @@
+const nombres = [1, 2, 3, 4, 5]; //Exemple de tableau
+let somme = 0;
+for (let i = 0; i< nombres, lenght; i++) {
+    somme += nombres[i];
+    
+}
+console.log("Somme du tableau :"somme);
