5ff4b8d9c50219e9070a4182d2f9dfdb482fda77
Samir Soula <samirsl@MacBook-de-Samir.local>
2025-05-17 00:37:42 +0200

    Ajout de l'exercice 3 : Somme de deux nombres

--- lvl1-ex3.js
+++ lvl1-ex3.js
@@ -1,3 +1,3 @@
 const nb1 = Number(prompt("Entrez le premier nombre :"));
 const nb2 = Number(prompt("Entrez le deuxième nombre :"));
-console.log("la somme est :" nb1 + nb2);
+console.log("La somme est :", nb1 + nb2);
