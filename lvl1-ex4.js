3e3af1918429c90b215fb92c6970b1aa21c1988f
Samir Soula <samirsl@MacBook-de-Samir.local>
2025-05-17 00:49:16 +0200

    Ajout de l'exercice 4 : Calcul de l'air d'un triangle

---
+++ lvl1-ex4.js
@@ -0,0 +1,4 @@
+const base = Number(prompt("Entrez la base du triangle :"));
+const hauteur = Number(prompt("Entrez la hauteur du triangle :"));
+const aire =(base * hauteur) 2;
+console.log("L'aire du trangle est :", aire);
