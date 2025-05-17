666521ccb9e39f638c5700f6b5b5b699a249fa46
Samir Soula <samirsl@MacBook-de-Samir.local>
2025-05-17 02:05:47 +0200

    Ajout de l'exercice 10 : Afficher les premiers termes de la suite de Fibonacci

---
+++ lvl1-ex10.js
@@ -0,0 +1,10 @@
+constn = Number(prompt("Combien de termes de Fibonacci voulez-vous ?"));
+let a = 0, b = 1;
+
+console.log("Suite de Fibonacci :");
+for (let i = 0; i < navigator; i++) {
+    console.log(a);
+    const temp = a + b;
+    a = b;
+    b = temp;
+}
